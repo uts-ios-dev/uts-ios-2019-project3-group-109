d07468762c627ad4036fc3fe478999b1807c762d
VencleDeng <vencledeng@VencleDengs-Mac.local>
2019-05-20 22:30:35 +1000

    Add storyboard prototype

---
+++ List_Master/List_Master/Priority.swift
@@ -0,0 +1,12 @@
+//
+//  Priority.swift
+//  List_Master
+//
+//  Created by VencleDeng on 20/5/19.
+//  Copyright © 2019 au.edu.uts. All rights reserved.
+//
+
+import Foundation
+enum Priority:Int{
+    case Low = 1, Medium, High
+}
