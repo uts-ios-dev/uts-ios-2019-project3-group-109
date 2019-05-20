d3bcd9eea1b7cdc6858158f9d0f02412db50fb80
VencleDeng <vencledeng@VencleDengs-Mac.local>
2019-05-20 19:37:59 +1000

    Initiate List_Master Project

---
+++ List_Master/List_Master/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  List_Master
+//
+//  Created by VencleDeng on 20/5/19.
+//  Copyright © 2019 au.edu.uts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
