042216240c83b7668380206361586c47ae3ff94a
Qi Zhang <13176767@student.uts.edu.au>
2019-05-28 04:11:29 +1000

    Category part bug fix
    Bug of undescribed category table cell fixed but runtime error exists on other parts

--- List_Master/List_Master/CategoryListViewController.swift
+++ List_Master/List_Master/CategoryListViewController.swift
@@ -7,6 +7,24 @@
 //
 import UIKit
 import Foundation
+
+class CategoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
 class CategoryListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     @IBOutlet weak var createCategory: UIBarButtonItem!
