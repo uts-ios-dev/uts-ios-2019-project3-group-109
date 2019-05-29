430b9fb1add7d9e209754f0b660915b6399ca45e
vencledeng <12486421@student.uts.edu.au>
2019-05-29 20:52:04 +1000

    Add search todo function

--- List_Master/List_Master/HomeView/HomeController.swift
+++ List_Master/List_Master/HomeView/HomeController.swift
@@ -13,8 +13,11 @@ class HomeController: UIViewController {
     @IBOutlet weak var listTable: UITableView!
     @IBOutlet weak var editButton: UIBarButtonItem!
     @IBOutlet weak var sortButton: UIBarButtonItem!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var isSorted = false
-    var shownTodoList: [TodoItem] = TodoList.todos
+    var isSearched = false
+    var shownTodoList = [TodoItem]()
     
     @IBAction func editAction(_ sender: UIBarButtonItem) {
         self.listTable.isEditing = !self.listTable.isEditing
@@ -32,12 +35,20 @@ class HomeController: UIViewController {
         }
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+        setupSearchBar()
+        TodoList.loadTodos()
+        shownTodoList = TodoList.todos
+    }
+    func setupTableView() {
         listTable.delegate = self
         listTable.dataSource = self
-        TodoList.loadTodos()
-        
+    }
+    func setupSearchBar() {
+        searchBar.delegate = self
     }
     override func viewDidAppear(_ animated: Bool) {
         TodoList.saveTodos()
@@ -57,21 +68,28 @@ class HomeController: UIViewController {
 
 
 }
-extension HomeController: UITableViewDataSource, UITableViewDelegate{
+extension HomeController: UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate{
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let moveObjectTemp = TodoList.todos[sourceIndexPath.item]
-        TodoList.todos.remove(at: sourceIndexPath.item)
-        TodoList.todos.insert(moveObjectTemp, at: destinationIndexPath.item)
-        TodoList.saveTodos()
+            let moveObjectTemp = TodoList.todos[sourceIndexPath.item]
+            TodoList.todos.remove(at: sourceIndexPath.item)
+            TodoList.todos.insert(moveObjectTemp, at: destinationIndexPath.item)
+            TodoList.saveTodos()
+       
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TodoList.todos.count
+        if !isSearched{
+            shownTodoList = isSorted ? TodoList.sortedTodos : TodoList.todos
+        }
+        return shownTodoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TodoTableViewCell
-        shownTodoList = isSorted ? TodoList.sortedTodos : TodoList.todos
+        if !isSearched{
+            shownTodoList = isSorted ? TodoList.sortedTodos : TodoList.todos
+        }
+        print(shownTodoList.count)
         cell.todoTitle.text = shownTodoList[indexPath.item].title
         cell.todoPriority.text = shownTodoList[indexPath.item].priority
         if shownTodoList[indexPath.row].completed {
@@ -97,5 +115,19 @@ extension HomeController: UITableViewDataSource, UITableViewDelegate{
        let todo = TodoList.todos[indexPath.row]
         performSegue(withIdentifier: "HomeToDetail", sender: todo.id)
     }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        guard !searchText.isEmpty else {
+            isSearched = false
+            listTable.reloadData()
+            return
+        }
+        isSearched = true
+        shownTodoList = shownTodoList.filter({ todo -> Bool in
+            todo.title.lowercased().contains(searchText.lowercased())
+        })
+        listTable.reloadData()
+    }
+    
     
 }
